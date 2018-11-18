9d14c758f0919528e7b0dcd91bd34e561bd97d66
Aran Ragu <aran07@hotmail.co.uk>
2018-11-18 16:13:02 +0000

    Initial commit

---
+++ src/route.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var React = require('react');
+
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
+
+var routes = (
+    <Route name="app" path="/" handler={require('./components/app')}>
+        <DefaultRoute handler={require('./components/HomePage')} />
+        <Route name="authors" handler={require('./components/authors/authorsPage')}/>
+        <Route name="addAuthor" path="author" handler={require('./components/authors/ManageAuthorPage')}/>
+        <Route name="manageAuthor" path="author/:id" handler={require('./components/authors/ManageAuthorPage')}/>
+        <Route name="about" handler={require('./components/about/aboutPage')}/>
+        <NotFoundRoute handler={require('./components/notFoundPage')} />
+        <Redirect from="about-us" to="about"/>
+        <Redirect from="about/*" to="about"/>
+    </Route>
+);
+
+module.exports = routes;
